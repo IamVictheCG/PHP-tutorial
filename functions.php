44adea7b4a76275a2125202e6cc130cdc13cfb17
Victor Okechukwu <VicOke360@gmail.com>
2023-03-25 18:26:42 +0100

    functions.php___updates made

--- functions.php
+++ functions.php
@@ -1,10 +1,21 @@
 <?php
-    $x = 10;
-    function registerUser(){
-        global $x;
-        echo $x;
-        echo 'Hi Victor, you have been registered';
-    }
+    // $x = 10;
+    // function registerUser(){
+    //     global $x;
+    //     echo $x;
+    //     echo 'Hi Victor, you have been registered';
+    // }
 
-    registerUser();
+    // function registerUser($name){
+    //     echo "Hi $name, you have been registered";
+    // }
+    // registerUser('Victor Okechukwu');
+
+    function sum($b1,$b2) {
+        // echo $b1 + $b2;
+        return $b1 + $b2;
+    }
+    // sum(4, 6);
+    echo sum(4, 6);
+
 ?>
